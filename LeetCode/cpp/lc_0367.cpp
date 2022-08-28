430961fcb305e6b97408a248695bca51391a6656
duycc <gz_duyong@163.com>
2022-08-28 22:01:31 +0800

    clnag-format

--- LeetCode/cpp/lc_0367.cpp
+++ LeetCode/cpp/lc_0367.cpp
@@ -5,25 +5,25 @@
  * @date     2021-09-05
  */
 class Solution {
-public:
-  bool isPerfectSquare(int num) {
-    if (num <= 0) {
-      return false;
+  public:
+    bool isPerfectSquare(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        int left  = 1;
+        int right = num;
+        int mid, sqrt;
+        while (left <= right) {
+            mid  = left + ((right - left) >> 1);
+            sqrt = num / mid;
+            if (sqrt == mid && num % sqrt == 0) { // 保证是整除
+                return true;
+            } else if (sqrt < mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
     }
-    int left = 1;
-    int right = num;
-    int mid, sqrt;
-    while (left <= right) {
-      mid = left + ((right - left) >> 1);
-      sqrt = num / mid;
-      if (sqrt == mid && num % sqrt == 0) { // 保证是整除
-        return true;
-      } else if (sqrt < mid) {
-        right = mid - 1;
-      } else {
-        left = mid + 1;
-      }
-    }
-    return false;
-  }
 };
